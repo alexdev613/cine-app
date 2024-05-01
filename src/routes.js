fbd3d1b6ee2e19a8868935463adb338d3c368dfb
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-05-01 15:29:04 -0300

    Creating and styling the project Header

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Filme from "./pages/Filme";
 
+import Header from "./components/Header";
+
 function RoutesApp() {
   return(
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={ <Home /> } />
-        <Route path="/pages/:id" element={ <Filme /> } />
+        <Route path="/filme/:id" element={ <Filme /> } />
       </Routes>  
     </BrowserRouter>
   )
