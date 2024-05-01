cd660dd340a5e84dca0a09f73c1843336500f7bf
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-05-01 20:19:06 -0300

    feat: I created and styled the not-found Error page and in routes.js I created the error page route. On the Home page I set a useState that will have the function of rendering a loading state.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,7 @@ import './home.css';
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -20,10 +21,19 @@ function Home() {
 
       console.log(response.data.results.slice(0, 10));
       setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
 
   return(
     <div className="container">
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home';
 import Filme from "./pages/Filme";
+import Erro from "./pages/Erro";
 
 import Header from "./components/Header";
 
@@ -12,6 +13,8 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/filme/:id" element={ <Filme /> } />
+
+        <Route path="*" element={ <Erro /> } />
       </Routes>  
     </BrowserRouter>
   )
