18b7420313cbce32e639a5a185c6b5bfa558e483
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-05-01 19:30:48 -0300

    feat: I configured useState filmes as an array, to set setFilmes to fetch 10 films from the results from the API, implemented a map to render the film title, the poster image and called the react-router-dom Link to set the path to the details page of the movie. I also styled this component on page Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import api from "../../services/api";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './home.css';
 
 // URL da API : /movie/now_playing?api_key=5da9e9ec1d460b54205a9798c1dab833&language=pt-BR
 
 function Home() {
-  const [filmes, setFIlmes] = useState([]);
+  const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -16,15 +18,28 @@ function Home() {
         }
       });
 
-      console.log(response.data.results);
+      console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10));
     }
 
     loadFilmes();
   }, []);
 
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              {/* <span>{filme.original_title} - ID: {filme.id}</span> */}
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}  alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+
     </div>
   )
 }
