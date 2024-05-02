d0cbb537b1d7aa7e96485c82b1a376021c7a8259
Alex Nascimento <alexjose68@hotmail.com>
2024-05-02 16:10:38 -0300

    feat: I created the Favorites page, and passed a route to it in routes.js, I did the logic to show favorite films by searching the data from what we have in localStorage, we created the function to exclude the film we want to delete from the list both visually how much from localStorage to the button 'Excluir'

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import './favoritos.css'
+import { Link } from 'react-router-dom';
+
+function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+
+    const minhaLista = localStorage.getItem("@primeflix");
+    setFilmes(JSON.parse(minhaLista) || []);
+
+  }, []);
+
+  function excluirFilme(id) {
+    alert("ID CLICADO " + id);
+    let filtroFilmes = filmes.filter( (item) => {
+      return (item.id !== id);
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+  }
+
+  return(
+    <div className='meus-filmes'>
+      <h1>Meus filmes favoritos</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo! :(</span>}
+        <ul>
+          {filmes.map((item) => {
+            return(
+              <li key={item.id}>
+                <span>{item.title}</span>
+
+                <div>
+                  <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                  <button onClick={ () => excluirFilme(item.id) }>Excluir</button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+    </div>
+  )
+}
+
+export default Favoritos;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home';
 import Filme from "./pages/Filme";
+import Favoritos from "./pages/Favoritos";
+
 import Erro from "./pages/Erro";
 
 import Header from "./components/Header";
@@ -13,6 +15,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/filme/:id" element={ <Filme /> } />
+        <Route path="/favoritos" element={ <Favoritos /> } />
 
         <Route path="*" element={ <Erro /> } />
       </Routes>  
