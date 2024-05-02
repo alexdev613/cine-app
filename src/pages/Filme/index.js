21882511e1be8cedda095bc5c8485aae996b5743
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-05-01 21:58:43 -0300

    feat: I worked on the Movie details page, implemented a useEffect to obtain the necessary data for the component from the movie ID through an async function, built and styled the component showing title, backdrop image, synopsis and the average of the movie and buttons.

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,9 +1,65 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import './filme-info.css';
+
+import api from '../../services/api';
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "5da9e9ec1d460b54205a9798c1dab833",
+          language: "pt-BR",
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+        console.log(response.data)
+      })
+      .catch(() => {
+        console.log("FILME NÃO ENCONTRADO");
+      })
+    }
+
+    loadFilme();
+
+    return () => {
+        console.log("COMPONENTE FOI DESMONTADO.");
+    }
+  }, []);
+
+  if(loading) {
     return(
-        <div>
-            <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
-        </div>
+      <div className='filme-info'>
+        <h1>Carregando detalhes...</h1>
+      </div>
     )
+  }
+  return(
+    <div className='filme-info'>
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average}/10</strong>
+
+      <div className='area-buttons'>
+        <button>Salvar</button>
+        <button>
+          <Link to={'#'}>
+            Trailer
+          </Link>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Filme;
